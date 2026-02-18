aa6cc352200a77522e9751b1ba576dfd1a0caabd
panutdoang <panutdoang98@gmail.com>
2026-02-18 13:50:01 +0000

    ketika pertama kali akses web, tolong dibuat dropdown teks mentah dan ha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PdfConverter from "@/components/pdf-converter";
 
 export default function Home() {
   return (
-    <main id="main-container" className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8">
+    <main id="main-container" className="flex min-h-screen flex-col items-center justify-center p-4">
       <div className="w-full max-w-5xl">
         <PdfConverter />
       </div>
